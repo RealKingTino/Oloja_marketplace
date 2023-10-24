942f29a89ec6fc989b8637f2e40fcb7d075b87cc
RealKingTino <tmowizzy@gmail.com>
2023-10-24 05:41:30 -0700

    Aliyu - product.py

---
+++ Flask/models/product.py
@@ -0,0 +1,45 @@
+from .base_model import BaseModel
+from models import db, Column, String, Integer, Float
+
+class Product(BaseModel):
+    """This class defines a product."""
+    __tablename__ = "products"
+
+    location = Column(String(256), nullable=False)
+    product_name = Column(String(256), nullable=False)
+    description = Column(String(1000), nullable=False)
+    image = Column(String(256), nullable=False)
+    category = Column(String(256), nullable=False)
+    price = Column(Float, nullable=False)
+    stock_quantity = Column(Integer, nullable=False)
+
+    def create(self, location, product_name, description, image, category, price, stock_quantity):
+        """Create a new product."""
+        self.location = location
+        self.product_name = product_name
+        self.description = description
+        self.image = image
+        self.category = category
+        self.price = price
+        self.stock_quantity = stock_quantity
+
+    def read(self, product_id):
+        """Read product details."""
+        pass
+
+    def update(self, product_id, **kwargs):
+        """Update product details."""
+        pass
+
+    def delete(self, product_id):
+        """Delete a product."""
+        pass
+
+    def calculate_discount(self, discount_percentage):
+        """Calculate the discount on the product price."""
+        discounted_price = self.price * (1 - discount_percentage / 100)
+        return discounted_price
+
+    def process_payment(self, payment_details):
+        """Process the payment for the product."""
+        pass
