410703d00717a6b3b5aad39a93e01afdc9155aad
RealKingTino <tmowizzy@gmail.com>
2023-11-01 03:27:19 -0700

    Aliyu -

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, DateTime, text, Integer, ForeignKey
+from sqlalchemy import Column, String, DateTime, text, Integer, ForeignKey, Float
 from sqlalchemy.orm import declarative_base
 
 app = Flask(__name__)
