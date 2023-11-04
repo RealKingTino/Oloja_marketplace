5a6ebc8d5cd71e026d247287bb80549d2d63ccda
Rashnotech <rashnotech@gmail.com>
2023-11-04 02:14:01 +0100

    Added registration

--- forms.py
+++ forms.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """ a module that contain forms"""
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField, TelField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     """ a class that contain the login form"""
     username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+class RegisterForm(FlaskForm):
+    """a class for registration form"""
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last name', validators=[DataRequired()])
+    telephone = TelField('Telephone', validators=[DataRequired])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField("Login")
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 """ a module that contain the auth routes"""
-from flask import flash, Blueprint, render_template
+from flask import flash, Blueprint, render_template, request, abort, jsonify, redirect, sessions
 from forms import LoginForm
-
+import requests
 auth_blueprint = Blueprint('auth_blueprint', __name__, template_folder='templates/auth')
 
 
-@auth_blueprint.route('/login', methods=['POST', 'GET'], strict_slashes=False)
+@auth_blueprint.route('/login', methods=['POST'], strict_slashes=False)
 def login():
     """ Login
         ------
         return an entry point to the user dashboard
     """
     return render_template("auth/login.html", form=LoginForm())
+
+
+@auth_blueprint.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth():
+    """ auth
+        ----
+        auth to the server
+    """
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    url = 'http://api.v1.dev.localhost:5000/signin'
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        response = response.json()
+        sessions.details = response
+        redirect('/dashboard')
+    abort(401, 'Unauthorized')
